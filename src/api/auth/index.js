a478fc7e1899afe571293df8dfae1e8e5d5d777f
Lauri Kajan <lauri.kajan@codento.com>
2018-03-12 09:33:44 +0200

    "beautify" code and "better" error handling

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -7,55 +7,60 @@ import ProfileService from '../../services/profiles'
 
 require('dotenv').config()
 
+class NotAuthorizedError extends Error {}
+class ServerError extends Error {}
+
 const router = Router()
 
 async function verify (idToken) {
-  const client = new OAuth2Client(process.env.CLIENT_ID)
+  const userService = new UserService()
+  const profileService = new ProfileService()
+  const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
+
   const ticket = await client.verifyIdToken({
     idToken: idToken,
-    audience: process.env.CLIENT_ID
+    audience: process.env.GOOGLE_CLIENT_ID
   })
+
   const ticketPayload = ticket.getPayload()
+  if (ticketPayload.hd !== 'codento.com') {
+    throw new NotAuthorizedError('Domain not authorized')
+  }
+
   const googleId = ticketPayload['sub']
 
-  if (ticketPayload.hd === 'codento.com') {
-    const userService = new UserService()
-    let user = await userService.getByGoogleId(googleId)
-    if (!user) {
-      user = await userService.create({
-        googleId: googleId,
-        firstName: ticketPayload.given_name,
-        lastName: ticketPayload.family_name,
-        active: true,
-        admin: false
-      })
-    }
-    if (!user) {
-      console.error('could not create user')
-    }
-    const profileService = new ProfileService()
-    const profile = await profileService.getByUserId(user.id)
-    const payload = {
-      googleId: user.googleId,
-      userId: user.id,
-      email: ticketPayload.email
-    }
-    const validTime = 3600 // 1h
-    let token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: validTime
+  let user = await userService.getByGoogleId(googleId)
+  if (!user) {
+    user = await userService.create({
+      googleId: googleId,
+      firstName: ticketPayload.given_name,
+      lastName: ticketPayload.family_name,
+      active: true,
+      admin: false
     })
-    let expires = new Date()
-    expires.setTime(expires.getTime() + validTime * 1000)
+    if (!user) {
+      throw new ServerError('Could not create user')
+    }
+  }
 
-    return {
-      success: true,
-      message: 'Welcome to rytmi app',
-      userId: user.id,
-      profileId: profile ? profile.id : null,
-      token: {
-        token: token,
-        expires: expires
-      }
+  const expires = Math.floor(new Date() / 1000) + parseInt(process.env.JWT_VALID_TIME)
+  const payload = {
+    googleId: user.googleId,
+    userId: user.id,
+    email: ticketPayload.email,
+    exp: expires
+  }
+  const token = jwt.sign(payload, process.env.JWT_SECRET)
+
+  const profile = await profileService.getByUserId(user.id)
+
+  return {
+    message: 'Welcome to rytmi app',
+    userId: user.id,
+    profileId: profile ? profile.id : null,
+    jwt: {
+      token: token,
+      expires: expires
     }
   }
 }
@@ -67,11 +72,16 @@ export default () => {
       res.status(400).json(utils.errorTemplate(400, 'Missing client id'))
     }
     verify(idToken)
-      .then((token) => {
-        if (token) {
-          res.json(token)
-        } else {
+      .then((authInfo) => {
+        res.json(authInfo)
+      })
+      .catch(err => {
+        if (err instanceof NotAuthorizedError) {
           res.status(403).json(utils.errorTemplate(403, 'Not authorized'))
+        } else if (err instanceof ServerError) {
+          res.status(500).json(utils.errorTemplate(500, 'Some horrible happened on a server side.', err.message))
+        } else {
+          res.status(400).json(utils.errorTemplate(400, 'Authentication error', err.message))
         }
       })
   })
