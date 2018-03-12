bd81d1fc4db7976f71f5a8aa7391291c34732283
Lauri Kajan <lauri.kajan@codento.com>
2018-03-12 09:43:44 +0200

    fix linting errors

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,34 +1,36 @@
 module.exports = (sequelize, DataTypes) => {
-  let User = sequelize.define('User', {
-    googleId: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    active: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false
-    },
-    admin: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false
-    }
-  },
-  {
-    indexes: [
-      {
-        unique: true,
-        fields: ['googleId']
+  let User = sequelize.define('User',
+    {
+      googleId: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+      },
+      admin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
       }
-    ]
-  })
+    },
+    {
+      indexes: [
+        {
+          unique: true,
+          fields: ['googleId']
+        }
+      ]
+    }
+  )
 
   User.associate = (models) => {
     models.User.hasOne(models.Profile, {foreignKey: 'userId'})
