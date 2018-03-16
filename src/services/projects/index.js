474d68a6690510afc4c0477d4bf5c6236043fd1d
Eero Pukkila <eero.pukkila@codento.com>
2018-03-16 18:11:01 +0200

    projects service started

---
+++ src/services/projects/index.js
@@ -0,0 +1,12 @@
+import CrudService from '../crud'
+import models from '../../db/models'
+
+export default class ProjectService extends CrudService {
+  constructor () {
+    super(models.Project)
+  }
+
+  getByProjectId (projectId) {
+    return models.Project.findOne({where: {id: projectId}})
+  }
+}
