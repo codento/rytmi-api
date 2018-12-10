697a6b459b80715e03d6d8607674b1f2fda022a6
Valtteri Lilja <valtteri.lilja@gmail.com>
2018-12-10 12:24:13 +0200

    Changed update, create and delete operations to exclude deleteAt-attribute

--- src/services/crud.js
+++ src/services/crud.js
@@ -23,12 +23,12 @@ export default class CrudService {
     return this.model
       .build(attrs)
       .save()
+      .then(created => this.get(created.id))
   }
 
   update (id, attrs) {
     attrs.id = parseInt(id)
-    return this.model
-      .findById(id)
+    return this.get(id)
       .then(modelInstance => {
         return modelInstance
           .update(attrs)
@@ -36,8 +36,7 @@ export default class CrudService {
   }
 
   delete (id) {
-    return this.model
-      .findById(id)
+    return this.get(id)
       .then(modelInstance => {
         return modelInstance
           .destroy()
