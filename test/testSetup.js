e8d85612f557e109b9ed1a92191345516bae97ba
Valtteri Lilja <valtteri.lilja@gmail.com>
2019-02-11 14:07:11 +0200

    Initialized test users to db in setup phase

--- test/testSetup.js
+++ test/testSetup.js
@@ -2,7 +2,17 @@ require('babel-register')
 require('babel-polyfill')
 
 const { migrationsUmzug } = require('./utils')
+const { user, profile } = require('../src/db/models/')
+const { users, profiles } = require('./mockData/mockUsers')
 
-module.exports = function () {
-  return migrationsUmzug.up()
+module.exports = async function () {
+  await executeMigrations()
+  return insertTestUsersWithProfiles()
+}
+
+const executeMigrations = () => migrationsUmzug.up()
+
+const insertTestUsersWithProfiles = async () => {
+  await user.bulkCreate(users)
+  await profile.bulkCreate(profiles)
 }
