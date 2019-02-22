11340b6737d49c5a7f52d574c8c4a288ac662887
Valtteri Lilja <valtteri.lilja@gmail.com>
2019-02-22 09:38:15 +0200

    Refactore profile skills test suite

--- test/api/profileSkills.spec.js
+++ test/api/profileSkills.spec.js
@@ -1,119 +1,74 @@
-import { generatePost, endpointAuthorizationTest } from '../utils'
+import { endpointAuthorizationTest } from '../utils'
 import app from '../../src/api/app'
 import supertest from 'supertest'
 import defaults from 'superagent-defaults'
-import { testAdminToken, invalidToken } from './tokens'
+import {
+  user as userModel,
+  skill as skillModel,
+  profileSkill as profileSkillModel
+} from '../../src/db/models'
+import { testUserToken, invalidToken } from './tokens'
 
 const request = defaults(supertest(app))
 
-const profileSkillEndpoint = '/api/profileSkills/'
-const profileEndpoint = '/api/profiles/'
-const skillEndpointFor =
-  profile => profileEndpoint + profile.id + '/skills/'
-const createProfile = generatePost(request, profileEndpoint)
-const createSkill = generatePost(request, '/api/skills/')
-const createUser = generatePost(request, '/api/users/')
-const createProfileSkill =
-  (profile, attrs) => generatePost(request, skillEndpointFor(profile))(attrs)
-const db = {}
+const profileSkillEndpoint = '/api/profileskills/'
 
-beforeAll(async done => {
-  request.set('Authorization', `Bearer ${testAdminToken}`)
-  request.set('Accept', 'application/json')
+describe('API profileskills endpoint', () => {
+  let profileSkills
+  beforeAll(async () => {
+    request.set('Accept', 'application/json')
+    request.set('Authorization', `Bearer ${testUserToken}`)
+    const [normalUser] = await userModel.findAll({ where: { admin: false } })
+    const [react, nodeJs] = await skillModel.findAll()
+    const nodeProfileSkill = {
+      description: 'so node',
+      knows: 2,
+      profileId: normalUser.id,
+      skillId: nodeJs.id,
+      visibleInCV: true,
+      wantsTo: 3
+    }
 
-  db.skill1 = await createSkill({
-    name: 'ArnoldC',
-    description: 'blah blah',
-    skillCategoryId: '1'
+    const reactProfileSkill = {
+      description: 'so node',
+      knows: 4,
+      profileId: normalUser.id,
+      skillId: react.id,
+      visibleInCV: true,
+      wantsTo: 5
+    }
+    profileSkills = [reactProfileSkill, nodeProfileSkill]
+    await profileSkillModel.bulkCreate(profileSkills)
   })
-  db.skill2 = await createSkill({
-    name: 'Brainfuck',
-    description: 'blah blah',
-    skillCategoryId: '1'
-  })
-  db.user1 = await createUser({
-    googleId: '8732157813573548',
-    firstName: 'tupu',
-    lastName: 'ankka',
-    active: true,
-    admin: true
-  })
-  db.user2 = await createUser({
-    googleId: '15648713281568',
-    firstName: 'hupu',
-    lastName: 'ankka',
-    active: true,
-    admin: true
-  })
-  db.user1Profile = await createProfile({
-    userId: db.user1.id,
-    lastName: 'Asdf',
-    firstName: 'Mr',
-    email: 'asdf@example.com',
-    active: true
-  })
-  db.user2Profile = await createProfile({
-    userId: db.user2.id,
-    lastName: 'Man',
-    firstName: 'Fdsa',
-    email: 'fdsa@example.com',
-    active: false
-  })
-  db.user1ProfileSkill1 = await createProfileSkill(db.user1Profile, {
-    profileId: db.user1Profile.id,
-    skillId: db.skill1.id,
-    knows: 5,
-    wantsTo: 1,
-    visibleInCV: true,
-    description: 'blah'
-  })
-  db.user1ProfileSkill2 = await createProfileSkill(db.user1Profile, {
-    profileId: db.user1Profile.id,
-    skillId: db.skill2.id,
-    knows: 3,
-    wantsTo: 0,
-    visibleInCV: true,
-    description: 'blah'
-  })
-  db.user2ProfileSkill = await createProfileSkill(db.user2Profile, {
-    profileId: db.user2Profile.id,
-    skillId: db.skill1.id,
-    knows: 0,
-    wantsTo: 5,
-    visibleInCV: true,
-    description: 'blah'
-  })
-  done()
-})
-
-describe('Fetching profileSkills', () => {
-  it('should return all profileSkills', async () => {
-    const all = await request
-      .get(profileSkillEndpoint)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200)
-    expect(all.body).toEqual(
-      expect.arrayContaining([
-        db.user1ProfileSkill1,
-        db.user1ProfileSkill2,
-        db.user2ProfileSkill
-      ]))
+  afterAll(async () => {
+    await profileSkillModel.destroy({ where: {}, truncate: true, force: true })
   })
 
-  it('should return profileSkill by id', async () => {
-    const profileSkill = await request
-      .get(profileSkillEndpoint + db.user1ProfileSkill1.id)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200)
-    expect(profileSkill.body).toMatchObject(db.user1ProfileSkill1)
+  describe('Fetching profile skills', () => {
+    it('should allow authorized user to fetch all profileskills', async () => {
+      const expectedProfileSkills = profileSkills
+      const response = await request.get(profileSkillEndpoint).expect(200)
+      expect(response.body.length).toBe(expectedProfileSkills.length)
+      response.body.forEach((profileSkill, idx) => {
+        expect(profileSkill).toMatchObject(expectedProfileSkills[idx])
+      })
+    })
+
+    it('should allow authorized user to fetch specific profileskill', async () => {
+      const [react] = await profileSkillModel.findAll()
+      const response = await request.get(profileSkillEndpoint + react.id).expect(200)
+      expect(response.body.id).toBe(react.id)
+      expect(response.body.description).toBe(react.description)
+      expect(response.body.skillId).toBe(react.skillId)
+      expect(response.body.profileId).toBe(react.profileId)
+      expect(response.body.wantsTo).toBe(react.wantsTo)
+      expect(response.body.knows).toBe(react.knows)
+    })
   })
-})
 
-describe('Endpoint authorization', () => {
-  request.set('Authorization', `Bearer ${invalidToken}`)
-
-  endpointAuthorizationTest(request.request.get, '/api/profileSkills/')
-  endpointAuthorizationTest(request.request.get, '/api/profileSkills/123')
+  describe('Endpoint authorization', () => {
+    request.set('Authorization', `Bearer ${invalidToken}`)
+    endpointAuthorizationTest(request.request.get, '/api/profileSkills/')
+    endpointAuthorizationTest(request.request.get, '/api/profileSkills/123')
+  })
 })
