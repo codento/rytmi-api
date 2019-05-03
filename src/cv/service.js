52610b4c5e40d14ef93518a1a8877e85b73f2567
Tuomas Valkeapää <tuomas.valkeapaa@codento.com>
2019-05-03 14:19:40 +0300

    WIP: VERY messy code. Testing stuff.

--- src/cv/service.js
+++ src/cv/service.js
@@ -1,40 +1,51 @@
 import { google } from 'googleapis'
 
-const mapData = (key, lists, template) => lists 
-  ? lists.map((item, i) => ({
+const mapData = (key, lists, template) => {
+  console.log('mapData: template')
+  console.log(template)
+  return lists
+    ? lists.map((item, i) => ({
       insertText: {
         text: `${item[0]}\t${item[1]}\n`,
         location: {
           index: getIndex(key, template.namedRanges, i)
         }
       }
-    })) 
-  : []
+    }))
+    : []
+}
 
-const getIndex = (key, namedRange, index) => namedRange[key].namedRanges[index].ranges[0].startIndex
+const getIndex = (key, namedRange, index) => {
+  console.log('getIndex: key, namedRange, index')
+  console.log(key)
+  console.log(namedRange)
+  console.log(index)
+  return namedRange[key].namedRanges[index].ranges[0].startIndex
+}
 
 // TODO -> configuration file
 const createMap = employeeData => ({
-    staticValues: [
-      { key: '{{ employeeName }}', newValue: employeeData.employeeName },
-      { key: '{{ employeeDescription }}', newValue: employeeData.employeeName },
-      { key: '{{ jobTitle }}', newValue: employeeData.jobTitle }
-    ],
-    lists: [
-      { key: '{{ topSkills }}', newValues: employeeData.topSkills },
-      { key: '{{ topProjects }}', newValues: employeeData.topProjects },
-      { key: '{{ skills }}', newValues: employeeData.skills },
-      { key: '{{ projects }}', newValues: employeeData.projects },
-      { key: '{{ languages }}', newValues: employeeData.languages }
-    ]
-  })
+  staticValues: [
+    { key: '{{ employeeName }}', newValue: employeeData.employeeName },
+    { key: '{{ employeeDescription }}', newValue: employeeData.employeeName },
+    { key: '{{ jobTitle }}', newValue: employeeData.jobTitle }
+  ],
+  lists: [
+    { key: '{{ topSkills }}', newValues: employeeData.topSkills },
+    { key: '{{ topProjects }}', newValues: employeeData.topProjects },
+    { key: '{{ skills }}', newValues: employeeData.skills },
+    { key: '{{ projects }}', newValues: employeeData.projects },
+    { key: '{{ languages }}', newValues: employeeData.languages }
+  ]
+})
 
 const templateId = '1If6AFJi8ip_yvyvTgzm5LgxiIXl3TUhvuEMFkcmQKQU' // TODO -> .env
 
 const create = async () => {
+  console.log('create')
   const auth = await google.auth.getClient({
     scopes: [
-        'https://www.googleapis.com/auth/drive',
+      'https://www.googleapis.com/auth/drive'
     ]
   })
 
@@ -43,26 +54,31 @@ const create = async () => {
     auth
   })
 
-  const { data } = await drive.files.copy({
+  const copy = await drive.files.copy({
     fileId: templateId,
     fields: ['id'],
     requestBody: {
-      name: 'Copied CV'
+      name: 'Copied CV',
+      parents: [{id: '1yI3Tla4i7N138GEX_hzT4K9a8onF_C93'}] // Lisätty testimielessä
     }
   })
 
-  setTimeout(async () => {
-  await drive.permissions.create({
-    fileId: data.id,
-    sendNotificationEmail: false,
-    requestBody: {
-      role: 'writer',
-      type: 'domain',
-      domain: 'codento.com'
-    }
-  })}
-  , 4000)
+  const data = copy.data
 
+  setTimeout(async () => {
+    await drive.permissions.create({
+      fileId: data.id,
+      sendNotificationEmail: false,
+      requestBody: {
+        role: 'writer',
+        type: 'user',
+        emailAddress: 'etunimi.sukunimi@codento.com'
+      }
+    })
+  }
+    , 4000)
+
+  console.log(data)
   return data
 }
 
@@ -78,33 +94,91 @@ const update = async (fileId, cv) => {
     auth
   })
 
-  const newValues = createMap(cv)
+  // const newValues = createMap(cv)
 
-  const staticRequests = newValues.staticValues.map(item => 
-    ({
-      replaceAllText: {
-        containsText: {
-          text: item.key,
-          matchCase: true
-        },
-        replaceText: item.newValue
+  // const staticRequests = newValues.staticValues.map(item =>
+  //   ({
+  //     replaceAllText: {
+  //       containsText: {
+  //         text: item.key,
+  //         matchCase: true
+  //       },
+  //       replaceText: item.newValue
+  //     }
+  //   })
+  // )
+
+  // const template = await slides.presentations.get({
+  //   presentationId: templateId
+  // })
+
+  // const listRequests = newValues.lists.map(item =>
+  //   mapData(item.key, item.newValues, template)).flat()
+
+  // const requests = listRequests.concat(staticRequests)
+
+  // await slides.presentations.batchUpdate({
+  //   presentationId: fileId,
+  //   requestBody: { requests }
+  // })
+
+  let topSkills = ''
+  cv.topSkills.forEach(skill => {
+    topSkills += skill.skillName + '\r\n'
+  })
+  const resource = {
+    'requests': [
+      {
+        'replaceAllText': {
+          'containsText': {
+            'text': '{{ jobTitle }}'
+          },
+          'replaceText': cv.jobTitle
+        }
+      },
+      {
+        'replaceAllText': {
+          'containsText': {
+            'text': '{{ topSkills }}'
+          },
+          'replaceText': topSkills
+        }
+      },
+      {
+        'replaceAllText': {
+          'containsText': {
+            'text': '{{ skillLevel11 }}'
+          },
+          'replaceText': (cv.skills[3] ? '' + cv.skills[3].skillLevel : '')
+        }
+      },
+      {
+        'replaceAllText': {
+          'containsText': {
+            'text': '{{ skillLevel10 }}'
+          },
+          'replaceText': (cv.skills[2] ? '' + cv.skills[2].skillLevel : '')
+        }
+      },
+      {
+        'replaceAllText': {
+          'containsText': {
+            'text': '{{ skillName11 }}'
+          },
+          'replaceText': (cv.skills[3] ? cv.skills[3].skillName : '')
+        }
+      },
+      {
+        'replaceAllText': {
+          'containsText': {
+            'text': '{{ skillName10 }}'
+          },
+          'replaceText': (cv.skills[2] ? cv.skills[2].skillName : '')
+        }
       }
-    })
-  )
-
-  const template = await slides.presentations.get({
-    presentationId: templateId
-  })
-
-  const listRequests = newValues.lists.map(item =>
-    mapData(item.key, item.newValues, template)).flat()
-  
-  const requests = listRequests.concat(staticRequests)
-
-  await slides.presentations.batchUpdate({
-    presentationId: fileId,
-    requestBody: { requests }
-  })
+    ]
+  }
+  slides.presentations.batchUpdate({resource, presentationId: fileId})
 }
 
 export default { create, update }
