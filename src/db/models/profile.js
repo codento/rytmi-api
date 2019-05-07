82716c13a26a370f60b21bfb15fee7bf141c8650
rnousia <riikka.nousiainen@codento.com>
2019-05-07 14:19:15 +0300

    Modify error message for phone number validator

--- src/db/models/profile.js
+++ src/db/models/profile.js
@@ -30,7 +30,7 @@ module.exports = (sequelize, DataTypes) => {
       validate: {
         is: {
           args: /^\+?[0-9 ()-]+$/i,
-          msg: 'Phone number must contain only numbers spaces or a plus sign'
+          msg: 'Phone number must contain only numbers, spaces, parentheses or a plus sign'
         }
       }
     },
