d68d7388c7deb4c50b40716ffb7019668ba67b6f
Jan Nyholm <jan.nyholm@codento.com>
2019-07-24 08:46:20 +0300

    fix unique constraint error

--- src/db/migrations/20190722080520-skill_field_translations.js
+++ src/db/migrations/20190722080520-skill_field_translations.js
@@ -14,21 +14,22 @@ module.exports = {
       await queryInterface.sequelize.query(
         `UPDATE "skillCategory" AS s
            SET "title" = json_build_object(
-             'fi', '',
+             'fi', (SELECT "oldTitle" FROM "skillGroup" c WHERE c."id" = s."id"),
              'en', (SELECT "oldTitle" FROM "skillCategory" c WHERE c."id" = s."id")
              )
         `, { transaction: transaction })
       await queryInterface.sequelize.query(
         `UPDATE "skillGroup" AS s
            SET "title" = json_build_object(
-             'fi', '',
+             'fi', (SELECT "oldTitle" FROM "skillGroup" c WHERE c."id" = s."id"),
              'en', (SELECT "oldTitle" FROM "skillGroup" c WHERE c."id" = s."id")
              )
         `, { transaction: transaction })
-      await queryInterface.sequelize.query(`CREATE UNIQUE INDEX skillCategory_title_fi_idx ON "skillCategory"( (title->>'en') )`, { transaction: transaction })
-      await queryInterface.sequelize.query(`CREATE UNIQUE INDEX skillCategory_title_en_idx ON "skillCategory"( (title->>'fi') )`, { transaction: transaction })
-      await queryInterface.sequelize.query(`CREATE UNIQUE INDEX skillGroup_title_en_idx ON "skillGroup"( (title->>'en') )`, { transaction: transaction })
+      await queryInterface.sequelize.query(`ALTER TABLE "skillCategory" DROP CONSTRAINT "SkillCategories_title_key"`, { transaction: transaction })
+      await queryInterface.sequelize.query(`CREATE UNIQUE INDEX skillCategory_title_fi_idx ON "skillCategory"( (title->>'fi') )`, { transaction: transaction })
+      await queryInterface.sequelize.query(`CREATE UNIQUE INDEX skillCategory_title_en_idx ON "skillCategory"( (title->>'en') )`, { transaction: transaction })
       await queryInterface.sequelize.query(`CREATE UNIQUE INDEX skillGroup_title_fi_idx ON "skillGroup"( (title->>'fi') )`, { transaction: transaction })
+      await queryInterface.sequelize.query(`CREATE UNIQUE INDEX skillGroup_title_en_idx ON "skillGroup"( (title->>'en') )`, { transaction: transaction })
       await queryInterface.removeColumn('skillCategory', 'oldTitle', { transaction: transaction })
       await queryInterface.removeColumn('skillGroup', 'oldTitle', { transaction: transaction })
     })
