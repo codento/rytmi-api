c4aec8d74bd2846fb276205da15fe39de0c970df
rnousia <riikka.nousiainen@codento.com>
2019-09-10 12:18:22 +0300

    Add translations to skills and languages

--- src/db/models/skill.js
+++ src/db/models/skill.js
@@ -1,11 +1,14 @@
+const langKeys = ['fi', 'en']
 module.exports = (sequelize, DataTypes) => {
   const Skill = sequelize.define('skill', {
     name: {
-      type: DataTypes.STRING,
+      type: DataTypes.JSONB,
       allowNull: false,
       unique: true
     },
-    description: DataTypes.TEXT,
+    description: {
+      type: DataTypes.JSONB
+    },
     skillCategoryId: {
       type: DataTypes.INTEGER,
       allowNull: false
@@ -13,6 +16,28 @@ module.exports = (sequelize, DataTypes) => {
   },
   {
     paranoid: true
+  }, {
+    validate: {
+      nameValidator: function () {
+        if (this.name) {
+          const keys = Object.keys(this.name)
+          if (!(keys.length === langKeys.length && keys.every(key => langKeys.includes(key)))) {
+            throw new Error(`Skill name json keys must be exactly: ${langKeys}`)
+          }
+          if (!(keys.every(key => this.name[key] && this.name[key].length > 0))) {
+            throw new Error('Skill name cannot be empty')
+          }
+        }
+      },
+      descriptionValidator: function () {
+        if (this.description) {
+          const keys = Object.keys(this.description)
+          if (!(keys.length === langKeys.length && keys.every(key => langKeys.includes(key)))) {
+            throw new Error(`Description json keys must be exactly: ${langKeys}`)
+          }
+        }
+      }
+    }
   })
 
   Skill.associate = (models) => {
