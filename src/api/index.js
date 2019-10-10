cc054115e8b0f0a43dfb6e17e7a32c79d4d9b02f
Jan Nyholm <jan.nyholm@codento.com>
2019-10-10 15:52:33 +0300

    set cron to run drive cleanup every night

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,17 @@
+import services from '../cv/tools/manageServiceAccountFiles'
 if (!global._babelPolyfill) {
   require('@babel/polyfill')
 }
+const cron = require('node-cron')
 const app = require('./app')
+
+// Run a cleaning function to remove cv-files from google drive
+if (process.env.NODE_ENV === 'production') {
+  cron.schedule('0 1 * * *', () => {
+    console.info('Running google drive cleanup')
+    services.deleteFiles()
+  })
+}
 
 let server = app.listen(process.env.PORT, () => {
   console.log(`Started on port ${server.address().port}`)
